c3e8fdb74f6b43a3d41e35674316a566d1728010
AkiteDonald <donaldakite27@gmail.com>
2022-12-06 16:42:40 +0000

    Re configure to add src tags automatically

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-    entry: {
-        index: './src/index.js',
-      },
-    devServer: {
-        static: './dist',
-      },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: './src/index.html',
-        }),
-      ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-      },
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ['style-loader',
-                  'css-loader'
-            ],
-          },
-        ],
-      },
 
-    mode: 'development'};
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  //   optimization: {
+  //     runtimeChunk: 'single',
+  //   },
+};
